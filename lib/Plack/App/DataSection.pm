03b3f2ac58eff638bc74ee2a186304cc3323655c
Masayuki Matsuki <y.songmu@gmail.com>
2012-08-20 00:52:28 +0900

    initial commit

---
+++ lib/Plack/App/DataSection.pm
@@ -0,0 +1,32 @@
+package Plack::App::DataSection;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::DataSection -
+
+=head1 SYNOPSIS
+
+  use Plack::App::DataSection;
+
+=head1 DESCRIPTION
+
+Plack::App::DataSection is
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
