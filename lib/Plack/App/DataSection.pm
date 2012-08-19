c3497ccd02e9ee8e1251bc9e2f2636945b24d752
Masayuki Matsuki <y.songmu@gmail.com>
2012-08-20 01:11:18 +0900

    copy from Plack::App::Directory::Markdown::Static

--- lib/Plack/App/DataSection.pm
+++ lib/Plack/App/DataSection.pm
@@ -3,6 +3,32 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use MIME::Base64;
+use Data::Section::Simple;
+use Plack::MIME;
+
+sub get_data {
+    my $file = shift;
+
+    my $content = get_data_section($file);
+    my $mime_type = Plack::MIME->mime_type($file);
+
+    if (is_binary($mime_type)) {
+        $content = decode_base64($content);
+    }
+    else {
+        $mime_type .= ' charset=UTF-8;';
+    }
+    ($content, $mime_type);
+}
+
+sub is_binary {
+    my $mime_type = shift;
+
+    $mime_type !~ /\b(?:text|xml)\b/;
+}
+
+
 1;
 __END__
 
